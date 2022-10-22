8b65c14cd1187cba034b9113602fdd76c2388767
lcaladoferreira <83848600+lcaladoferreira@users.noreply.github.com>
2022-10-22 20:06:26 -0300

    Static FE MUI

--- agenda-react/src/app/CalendarScreen.tsx
+++ agenda-react/src/app/CalendarScreen.tsx
@@ -1,3 +1,4 @@
+import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -5,21 +6,20 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import Button from "@material-ui/core/Button";
+import { FormControlLabel, Icon, IconButton } from "@material-ui/core";
+import Checkbox from "@material-ui/core/Checkbox";
+import Avatar from "@material-ui/core/Avatar";
 
 const DAYS_OF_WEEK = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"];
 const WEEK0 = [1, 2, 3, 4, 5, 6, 7];
-const WEEK1 = [8, 9, 10, 11, 12, 13, 14];
-const WEEK2 = [15, 16, 17, 18, 19, 20, 21];
-const WEEK3 = [22, 23, 24, 25, 26, 27, 28];
 const d = new Date();
 let date = d.getDate();
 console.log(date);
 
 const useStyles = makeStyles({
-  root: {
-    height: "100%",
-  },
   table: {
+    borderTop: "1px solid rgb(224, 224, 224)",
     minHeight: "100%",
     "& td ~ td, & th ~ th": {
       borderLeft: "1px solid rgb(224, 224, 224)",
@@ -31,48 +31,59 @@ export function CalendarScreen() {
   const classes = useStyles();
 
   return (
-    <TableContainer className={classes.root} component={"div"}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {DAYS_OF_WEEK.map((day) => (
-              <TableCell align="center" key={day}>
-                {day}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            {WEEK0.map((week) => (
-              <TableCell align="center" key={week}>
-                {week}
-              </TableCell>
-            ))}
-          </TableRow>
-          <TableRow>
-            {WEEK1.map((week) => (
-              <TableCell align="center" key={week}>
-                {week}
-              </TableCell>
-            ))}
-          </TableRow>
-          <TableRow>
-            {WEEK2.map((week) => (
-              <TableCell align="center" key={week}>
-                {week}
-              </TableCell>
-            ))}
-          </TableRow>
-          <TableRow>
-            {WEEK3.map((week) => (
-              <TableCell align="center" key={week}>
-                {week}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box display="flex" height="100%" alignItems="stretch">
+      <Box borderRight="1px solid rgb(224, 224, 224)" width="16em" padding="8px 16px">
+        <h2>Agenda React</h2>
+        <Button variant="contained" color="primary">
+          Novo Evento
+        </Button>
+
+        <Box marginTop="64px">
+          <h3>Agendas</h3>
+          <FormControlLabel control={<Checkbox />} label="Pessoal" />
+          <FormControlLabel control={<Checkbox />} label="Trabalho" />
+        </Box>
+      </Box>
+      <TableContainer component={"div"}>
+        <Box display="flex" alignItems="center" padding="8px, 16px">
+          <Box>
+            <IconButton aria-label="Mês anterior ">
+              <Icon>chevron_left</Icon>
+            </IconButton>
+            <IconButton aria-label="Próximo mês">
+              <Icon>chevron_right</Icon>
+            </IconButton>
+            <Box flex="1" marginLeft="16px" component="h3">
+              Mes
+            </Box>
+          </Box>
+          <IconButton aria-label="Usuário">
+            <Avatar>
+              <Icon>person</Icon>
+            </Avatar>
+          </IconButton>
+        </Box>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              {DAYS_OF_WEEK.map((day) => (
+                <TableCell align="center" key={day}>
+                  {day}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              {WEEK0.map((week) => (
+                <TableCell align="center" key={week}>
+                  {week}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
